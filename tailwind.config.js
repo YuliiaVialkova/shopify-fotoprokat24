d5cf243a93c14ce33727462d95d21292d9c6df26
Yuliia Vialkova <julia.zagrafova@gmail.com>
2025-11-09 22:38:49 +0200

    styled hero container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{liquid,json,html,js}"],
-  theme: { extend: {} },
+  content: [
+    "./layout/**/*.{liquid,json}",
+    "./templates/**/*.{liquid,json}",
+    "./sections/**/*.{liquid,json}",
+    "./snippets/**/*.{liquid,json}",
+    "./assets/**/*.{liquid,json,js}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
