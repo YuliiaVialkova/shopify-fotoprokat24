152ac678e8490ab02086efd8eac39955c795b60e
Yuliia Vialkova <julia.zagrafova@gmail.com>
2025-11-21 16:15:49 +0200

    deleted all variables from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,7 @@
 module.exports = {
   content: ["./**/*.liquid", "./**/*.json", "./**/*.html", "./**/*.js"],
   theme: {
-    extend: {
-      colors: {
-        main: "#000000",
-        secondary: "#75758A",
-        accent: "#60B021",
-        "btn-main": "#000000",
-        "btn-secondary": "#FFFFFF",
-        border: {
-          main: "#EFEFEF",
-        },
-      },
-      backgroundImage: {
-        "btn-secondary": "linear-gradient(180deg, #8EE902 0%, #4F9C2C 100%)",
-      },
-      backgroundColor: {
-        main: "#FFFFFF",
-        secondary: "#F5F5F5",
-        additional: "#4F9C2C",
-        "btn-main": "#FFFFFF",
-        "btn-additional": "#4F9C2C",
-      },
-    },
+    extend: {},
   },
   plugins: [],
   variantGroups: true,
